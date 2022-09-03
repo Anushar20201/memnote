604244f4242d7304ca166cc31b3b8dd8fd1eda81
Anusha <anushar20201@gmail.com>
2022-09-02 20:45:59 -0500

    adding getDb to get the content from the jate database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+}
+
+// console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (value) => {
+
+// console.error('getDb not implemented');
+// connecting to the jate db and mentioning the version 1 here
+const db = await openDB("jate", 1);
+//creating a tran for jate db (for getting setting the previliges to read)
+const tran = db.transaction("jate", "readonly");
+const store = tran.objectStore("jate");
+//assigning the getAll request here
+const request = store.getAll();
+//collecting the result
+const result = await request;
+console.log("This is the data from JATE db", result);
+}
 
 initdb();
