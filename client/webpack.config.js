a86a3f62b2474649ab61dd740421a1022a000791
Anusha <anushar20201@gmail.com>
2022-09-02 20:21:11 -0500

    adding plugins array for PWA

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,13 +17,61 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+
+    //plugins array for PWA
     plugins: [
+      // Adding HtmlWebpackPlugin simplifies creation of HTML files to serve webpack bundles
+     //making sure this appears before WebpackPwaManifest as we are using inject in our configuration
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      // Adding InjectManifest for compiling a service worker file provided via swSrc
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      //WebpackPwaManifest pluggin  generates a 'manifest.json' for your Progressive Web Application
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'memnote',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor!',
+        background_color: '##bfd6f2',
+        theme_color: '##bfd6f2',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [32, 48, 64, 96, 128, 192],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
       
     ],
 
     module: {
       rules: [
-        
+                {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            //babel-loader allows our webpack to process different types of files and then convert them into valid modules
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            },
+          },
+        }
       ],
     },
   };
